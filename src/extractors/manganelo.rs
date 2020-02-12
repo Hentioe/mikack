fc0cc92e87cbb0a5c8bfdf1b944ea509950672cd
Hentioe <me@bluerain.io>
2020-02-12 17:10:02 -0400

    Supports the Manganelo search

--- src/extractors/manganelo.rs
+++ src/extractors/manganelo.rs
@@ -1,27 +1,34 @@
 use super::*;
 
 /// 对 manganelo.com 内容的抓取实现
-def_extractor! {[usable: true, searchable: false],
+def_extractor! {[usable: true, pageable: true, searchable: true],
     fn index(&self, page: u32) -> Result<Vec<Comic>> {
-        let url = urlgen![
-            :first  => &"https://manganelo.com/genre-all?type=topview",
-            :next   => &"https://manganelo.com/genre-all/{}?type=topview",
-            :page   => &page
-        ];
+        let url = format!("https://manganelo.com/genre-all/{}", page);
 
-        itemsgen![
-            :entry          => Comic,
-            :url            => &url,
-            :target         => &r#".content-genres-item h3 > a.genres-item-name"#
-        ]
+        itemsgen2!(
+            url             = &url,
+            parent_dom      = ".content-genres-item",
+            cover_dom       = "img.img-loading",
+            link_dom        = "a.genres-item-name"
+        )
+    }
+
+    fn search(&self, keywords: &str) -> Result<Vec<Comic>> {
+        let url = format!("https://manganelo.com/search/{}", keywords);
+
+        itemsgen2!(
+            url             = &url,
+            parent_dom      = ".search-story-item",
+            cover_dom       = "img.img-loading",
+            link_dom        = "a.item-title"
+        )
     }
 
     fn fetch_chapters(&self, comic: &mut Comic) -> Result<()> {
-        itemsgen![
-            :entry          => Chapter,
-            :url            => &comic.url,
-            :target         => &".row-content-chapter > li > a.chapter-name"
-        ]?.reversed_attach_to(comic);
+        itemsgen2!(
+            url             = &comic.url,
+            target_dom      = ".row-content-chapter > li > a.chapter-name"
+        )?.reversed_attach_to(comic);
 
         Ok(())
     }
@@ -31,26 +38,34 @@ def_extractor! {[usable: true, searchable: false],
         let document = parse_document(&html);
         chapter.title = document.dom_text(".panel-chapter-info-top > h1")?;
         let addresses = document.dom_attrs(".container-chapter-reader > img", "src")?;
-        Ok(ChapterPages::full(chapter,  addresses))
+        Ok(ChapterPages::full(chapter, addresses))
     }
 }
 
 #[test]
 fn test_extr() {
     let extr = new_extr();
-    let comics = extr.index(1).unwrap();
-    assert_eq!(24, comics.len());
+    if extr.is_usable() {
+        let comics = extr.index(1).unwrap();
+        assert_eq!(24, comics.len());
 
-    let mut comic = Comic::new("Goblin Slayer", "https://manganelo.com/manga/hgj2047065412");
-    extr.fetch_chapters(&mut comic).unwrap();
-    assert_eq!(45, comic.chapters.len());
+        let mut comic1 = Comic::new(
+            "Naruto",
+            "https://manganelo.com/manga/read_naruto_manga_online_free3",
+        );
+        extr.fetch_chapters(&mut comic1).unwrap();
+        assert_eq!(748, comic1.chapters.len());
 
-    let chapter1 = &mut Chapter::new(
-        "",
-        "https://manganelo.com/chapter/hgj2047065412/chapter_1",
-        0,
-    );
-    extr.fetch_pages(chapter1).unwrap();
-    assert_eq!("GOBLIN SLAYER CHAPTER 1", chapter1.title);
-    assert_eq!(50, chapter1.pages.len());
+        let chapter1 = &mut comic1.chapters[0];
+        extr.fetch_pages_unsafe(chapter1).unwrap();
+        assert_eq!(
+            "NARUTO VOL.1 CHAPTER 0 : NARUTO PILOT MANGA",
+            chapter1.title
+        );
+        assert_eq!(46, chapter1.pages.len());
+        let comics = extr.search("Naruto").unwrap();
+        assert!(comics.len() > 0);
+        assert_eq!(comics[0].title, comic1.title);
+        assert_eq!(comics[0].url, comic1.url);
+    }
 }
