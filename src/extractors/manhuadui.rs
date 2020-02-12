69575557ed8d62771a4ea24561cd0ed60477bfb6
Hentioe <me@bluerain.io>
2020-02-12 17:45:41 -0400

    Supports the manhuadui search

--- src/extractors/manhuadui.rs
+++ src/extractors/manhuadui.rs
@@ -1,29 +1,40 @@
 use super::*;
 
-def_regex! [
-    ENCODE_TEXT_RE => r#"var\s*chapterImages\s*=\s*"([^"]+)""#,
-    PATH_RE        => r#"var\s*chapterPath\s*=\s*"([^"]+)""#
+def_regex2! [
+    ENCODE_TEXT => r#"var\s*chapterImages\s*=\s*"([^"]+)""#,
+    PATH        => r#"var\s*chapterPath\s*=\s*"([^"]+)""#
 ];
 
-def_extractor! {[usable: true, searchable: false],
+def_extractor! {[usable: true, pageable: true, searchable: true],
     fn index(&self, page: u32) -> Result<Vec<Comic>> {
-        let url = format!("https://www.manhuadui.com/list/riben/{}/", page);
+        let url = format!("https://www.manhuadui.com/update/{}/", page);
 
-        itemsgen![
-            :entry      => Comic,
-            :url        => &url,
-            :target     => &"ul.list_con_li > li h3> a"
-        ]
+        itemsgen2!(
+            url         = &url,
+            parent_dom  = ".list_con_li > li",
+            cover_dom   = ".comic_img > img",
+            link_dom    = "h3 > a"
+        )
+    }
+
+    fn search(&self, keywords: &str) -> Result<Vec<Comic>> {
+        let url = format!("https://www.manhuadui.com/search/?keywords={}", keywords);
+
+        itemsgen2!(
+            url         = &url,
+            parent_dom  = ".list_con_li > li",
+            cover_dom   = ".image-link > img",
+            link_dom    = "p > a"
+        )
     }
 
     fn fetch_chapters(&self, comic: &mut Comic) -> Result<()> {
-        itemsgen![
-            :entry          => Chapter,
-            :url            => &comic.url,
-            :target         => &"ul.list_con_li > li > a",
-            :href_prefix    => &"https://www.manhuadui.com",
-            :sub_dom_text   => &"span.list_con_zj"
-        ]?.attach_to(comic);
+        itemsgen2!(
+            url             = &comic.url,
+            target_dom      = "ul.list_con_li > li > a",
+            link_prefix     = "https://www.manhuadui.com",
+            link_text_dom   = "span.list_con_zj"
+        )?.attach_to(comic);
 
         Ok(())
     }
@@ -35,10 +46,7 @@ def_extractor! {[usable: true, searchable: false],
         let chapter_num = document.dom_text(".head_title > h2")?;
         chapter.title = format!("{} {}", comic_name, chapter_num);
 
-        let encode_text = match_content![
-            :text   => &html,
-            :regex  => &*ENCODE_TEXT_RE
-        ];
+        let encode_text = match_content2!(&html, &*ENCODE_TEXT_RE)?;
 
         let runtime = include_str!("../../runtime/manhuadui.js");
         let wrap_code = wrap_code!(runtime, format!("
@@ -48,10 +56,7 @@ def_extractor! {[usable: true, searchable: false],
             decrypt20180904(chapterImages)
         ", encode_text), :end);
 
-        let path = match_content![
-            :text   => &html,
-            :regex  => &*PATH_RE
-        ];
+        let path = match_content2!(&html, &*PATH_RE)?;
 
         let mut addresses = vec![];
         for fname in eval_value(&wrap_code)?.as_array()? {
@@ -66,22 +71,24 @@ def_extractor! {[usable: true, searchable: false],
 #[test]
 fn test_extr() {
     let extr = new_extr();
-    let comics = extr.index(1).unwrap();
-    assert_eq!(36, comics.len());
+    if extr.is_usable() {
+        let comics = extr.index(1).unwrap();
+        assert_eq!(36, comics.len());
 
-    let mut comic = Comic::from_link(
-        "境界触发者",
-        "https://www.manhuadui.com/manhua/jingjiechufazhe/",
-    );
-    extr.fetch_chapters(&mut comic).unwrap();
-    assert_eq!(190, comic.chapters.len());
-
-    let chapter1 = &mut Chapter::new(
-        "",
-        "https://www.manhuadui.com/manhua/jingjiechufazhe/435634.html",
-        0,
-    );
-    extr.fetch_pages(chapter1).unwrap();
-    assert_eq!("境界触发者 189话", chapter1.title);
-    assert_eq!(20, chapter1.pages.len());
+        let mut comic1 =
+            Comic::from_link("源君物语", "https://www.manhuadui.com/manhua/yuanjunwuyu/");
+        extr.fetch_chapters(&mut comic1).unwrap();
+        assert_eq!(358, comic1.chapters.len());
+        let chapter1 = &mut Chapter::from_link(
+            "",
+            "https://www.manhuadui.com/manhua/jingjiechufazhe/435634.html",
+        );
+        extr.fetch_pages_unsafe(chapter1).unwrap();
+        assert_eq!("境界触发者 189话", chapter1.title);
+        assert_eq!(20, chapter1.pages.len());
+        let comics = extr.search("源君物语").unwrap();
+        assert!(comics.len() > 0);
+        assert_eq!(comics[0].title, comic1.title);
+        assert_eq!(comics[0].url, comic1.url);
+    }
 }
