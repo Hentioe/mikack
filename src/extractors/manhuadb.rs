b9fbca2be021cc685fa2b116844c18e6d31b2e39
Hentioe <me@bluerain.io>
2020-02-12 17:24:36 -0400

    Supports the manhuadb search

--- src/extractors/manhuadb.rs
+++ src/extractors/manhuadb.rs
@@ -2,33 +2,43 @@ use super::*;
 use base64;
 use std::str;
 
-def_regex! [
-    CHAPTER_ID_RE   => r#"@type":"ListItem","position":3,"name":"([^"]+)""#,
-    IMG_DATA_RE     => r#"<script>\s?var img_data =\s+'([^']+)';</script>"#
+def_regex2! [
+    CHAPTER_ID   => r#"@type":"ListItem","position":3,"name":"([^"]+)""#,
+    IMG_DATA     => r#"<script>\s?var img_data =\s+'([^']+)';</script>"#
 ];
 
 /// 对 www.manhuadb.com 内容的抓取实现
-def_extractor! {[usable: true, searchable: false],
+def_extractor! {[usable: true, pageable: true, searchable: true],
     fn index(&self, page: u32) -> Result<Vec<Comic>> {
-        let url =
-            format!("https://www.manhuadb.com/update_{}.html", page);
+        let url = format!("https://www.manhuadb.com/update_{}.html", page);
 
-        itemsgen![
-            :entry          => Comic,
-            :url            => &url,
-            :href_prefix    => &"https://www.manhuadb.com",
-            :target         => &r#".comicbook-index  > a"#,
-            :text_attr      => &"title"
-        ]
+        itemsgen2!(
+            url             = &url,
+            parent_dom      = ".comicbook-index",
+            cover_dom       = ".img-fluid",
+            link_dom        = ".one-line > a",
+            link_prefix     = "https://www.manhuadb.com"
+        )
+    }
+
+    fn search(&self, keywords: &str) -> Result<Vec<Comic>> {
+        let url = format!("https://www.manhuadb.com/search?q={}", keywords);
+
+        itemsgen2!(
+            url             = &url,
+            parent_dom      = ".comicbook-index",
+            cover_dom       = ".img-fluid",
+            link_dom        = ".one-line > a",
+            link_prefix     = "https://www.manhuadb.com"
+        )
     }
 
     fn fetch_chapters(&self, comic: &mut Comic) -> Result<()> {
-        itemsgen![
-            :entry          => Chapter,
-            :url            => &comic.url,
-            :href_prefix    => &"https://www.manhuadb.com",
-            :target         => &r#".sort_div > a"#
-        ]?.attach_to(comic);
+        itemsgen2!(
+            url             = &comic.url,
+            target_dom      = ".sort_div > a",
+            link_prefix     = "https://www.manhuadb.com"
+        )?.attach_to(comic);
 
         Ok(())
     }
@@ -36,18 +46,12 @@ def_extractor! {[usable: true, searchable: false],
     fn pages_iter<'a>(&'a self, chapter: &'a mut Chapter) -> Result<ChapterPages> {
         let html = get(&chapter.url)?.text()?;
         let document = parse_document(&html);
-        let chapter_id = match_content![
-            :text   => &html,
-            :regex  => &*CHAPTER_ID_RE
-        ];
+        let chapter_id = match_content2!(&html, &*CHAPTER_ID_RE)?;
         let name = document.dom_text("h1 > a")?;
         chapter.title = format!("{} {}", name, chapter_id);
         let path_prefix = document.dom_attr("div[data-img_pre]", "data-img_pre")?;
-        let img_data = match_content![
-            :text   => &html,
-            :regex  => &*IMG_DATA_RE
-        ];
-        let decoded_img_data_bytes = &base64::decode(img_data)?[..];
+        let img_data = match_content2!(&html, &*IMG_DATA_RE)?;
+        let decoded_img_data_bytes = &base64::decode(&img_data)?[..];
         let decoded_img_data = str::from_utf8(decoded_img_data_bytes)?;
         let wrap_code = format!("
             var imgs = eval('{decoded_img_data}').map(item => item.img);
@@ -68,12 +72,16 @@ fn test_extr() {
     if extr.is_usable() {
         let comics = extr.index(1).unwrap();
         assert_eq!(36, comics.len());
-        let comic = &mut Comic::from_link("胜负师传说", "https://www.manhuadb.com/manhua/10906");
-        extr.fetch_chapters(comic).unwrap();
-        assert_eq!(41, comic.chapters.len());
-        let chapter1 = &mut comic.chapters[0];
+        let comic1 = &mut Comic::from_link("胜负师传说", "https://www.manhuadb.com/manhua/10906");
+        extr.fetch_chapters(comic1).unwrap();
+        assert_eq!(41, comic1.chapters.len());
+        let chapter1 = &mut comic1.chapters[0];
         extr.fetch_pages_unsafe(chapter1).unwrap();
         assert_eq!("胜负师传说 VOL01", chapter1.title);
         assert_eq!(194, chapter1.pages.len());
+        let comics = extr.search("胜负师传说").unwrap();
+        assert!(comics.len() > 0);
+        assert_eq!(comics[0].title, comic1.title);
+        assert_eq!(comics[0].url, comic1.url);
     }
 }
