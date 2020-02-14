0412adc396d6289ff46255578aacd7f2496caed1
Hentioe <me@bluerain.io>
2020-02-14 13:49:09 -0400

    Update the Luscious

--- src/extractors/luscious.rs
+++ src/extractors/luscious.rs
@@ -1,5 +1,6 @@
 use super::*;
 use serde::Deserialize;
+use serde_json::Value;
 
 #[derive(Debug, Deserialize)]
 struct Cover {
@@ -23,36 +24,20 @@ impl From<&ComicItem> for Comic {
     }
 }
 
-#[derive(Debug, Deserialize)]
-struct List {
-    items: Vec<ComicItem>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Album {
-    list: List,
-}
-
-#[derive(Debug, Deserialize)]
-struct Data {
-    album: Album,
-}
-
-#[derive(Debug, Deserialize)]
-struct Json {
-    data: Data,
-}
-
+/// 对 www.luscious.net 内容的抓取实现
 def_extractor! {[usable: true, pageable: true, searchable: false],
     fn index(&self, page: u32) -> Result<Vec<Comic>> {
         let mut url = String::from(r#"https://api.luscious.net/graphql/nobatch/?operationName=AlbumList&query=+query+AlbumList($input:+AlbumListInput!)+{+album+{+list(input:+$input)+{+info+{+...FacetCollectionInfo+}+items+{+...AlbumMinimal+}+}+}+}+fragment+FacetCollectionInfo+on+FacetCollectionInfo+{+page+has_next_page+has_previous_page+total_items+total_pages+items_per_page+url_complete+}+fragment+AlbumMinimal+on+Album+{+__typename+id+title+labels+description+created+modified+like_status+status+number_of_favorites+number_of_dislikes+number_of_pictures+number_of_animated_pictures+number_of_duplicates+slug+is_manga+url+download_url+permissions+created_by+{+id+url+name+display_name+user_title+avatar+{+url+size+}+}+cover+{+width+height+size+url+}+content+{+id+title+url+}+language+{+id+title+url+}+tags+{+id+category+text+url+count+}+genres+{+id+title+slug+url+}+audiences+{+id+title+url+}+}+&variables={"input":{"display":"date_trending","filters":[{"name":"album_type","value":"manga"}],"page":"#);
         url.push_str(&page.to_string());
         url.push_str("}}");
-        let json = get(&url)?.json::<Json>()?;
-        let mut comics = vec![];
-        for item in json.data.album.list.items {
-            comics.push(Comic::from(&item))
-        }
+        let json_v = get(&url)?.json::<Value>()?;
+        let list = json_v["data"]["album"]["list"]["items"].clone();
+        let comics = serde_json::from_value::<Vec<ComicItem>>(list)?
+            .iter()
+            .map(|c: &ComicItem| {
+                Comic::from(c)
+            })
+            .collect::<Vec<_>>();
 
         Ok(comics)
     }
