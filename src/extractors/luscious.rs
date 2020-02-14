30a9b11555a107556312383d0ebe3b71efcfb3b5
Hentioe <me@bluerain.io>
2020-02-14 07:30:22 -0400

    Update luscious

--- src/extractors/luscious.rs
+++ src/extractors/luscious.rs
@@ -7,15 +7,25 @@ struct Cover {
 }
 
 #[derive(Debug, Deserialize)]
-struct Item {
+struct ComicItem {
     title: String,
     url: String,
     cover: Cover,
 }
 
+impl From<&ComicItem> for Comic {
+    fn from(c: &ComicItem) -> Self {
+        Self::from_index(
+            &c.title,
+            &format!("https://www.luscious.net{}", &c.url),
+            &c.cover.url,
+        )
+    }
+}
+
 #[derive(Debug, Deserialize)]
 struct List {
-    items: Vec<Item>,
+    items: Vec<ComicItem>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -33,7 +43,7 @@ struct Json {
     data: Data,
 }
 
-def_extractor! {[usable: true, searchable: false],
+def_extractor! {[usable: true, pageable: true, searchable: true],
     fn index(&self, page: u32) -> Result<Vec<Comic>> {
         let mut url = String::from(r#"https://api.luscious.net/graphql/nobatch/?operationName=AlbumList&query=+query+AlbumList($input:+AlbumListInput!)+{+album+{+list(input:+$input)+{+info+{+...FacetCollectionInfo+}+items+{+...AlbumMinimal+}+}+}+}+fragment+FacetCollectionInfo+on+FacetCollectionInfo+{+page+has_next_page+has_previous_page+total_items+total_pages+items_per_page+url_complete+}+fragment+AlbumMinimal+on+Album+{+__typename+id+title+labels+description+created+modified+like_status+status+number_of_favorites+number_of_dislikes+number_of_pictures+number_of_animated_pictures+number_of_duplicates+slug+is_manga+url+download_url+permissions+created_by+{+id+url+name+display_name+user_title+avatar+{+url+size+}+}+cover+{+width+height+size+url+}+content+{+id+title+url+}+language+{+id+title+url+}+tags+{+id+category+text+url+count+}+genres+{+id+title+slug+url+}+audiences+{+id+title+url+}+}+&variables={"input":{"display":"date_trending","filters":[{"name":"album_type","value":"manga"}],"page":"#);
         url.push_str(&page.to_string());
@@ -41,14 +51,15 @@ def_extractor! {[usable: true, searchable: false],
         let json = get(&url)?.json::<Json>()?;
         let mut comics = vec![];
         for item in json.data.album.list.items {
-            comics.push(Comic::from_index(item.title, format!("https://www.luscious.net{}", item.url), item.cover.url))
+            comics.push(Comic::from(&item))
         }
 
         Ok(comics)
     }
 
     fn fetch_chapters(&self, comic: &mut Comic) -> Result<()> {
-        comic.push_chapter(Chapter::from_link(comic.title.clone(), comic.url.clone()));
+        comic.push_chapter(Chapter::from(&*comic));
+
         Ok(())
     }
 
@@ -77,11 +88,19 @@ fn test_extr() {
     let comics = extr.index(1).unwrap();
     assert_eq!(30, comics.len());
 
-    let chapter1 = &mut Chapter::from_link("", "https://www.luscious.net/albums/teitoku-wa-semai-toko-suki-kantai-collection-kanco_363520/");
+    let comic1 = Comic::from_link(
+        "Teitoku wa Semai Toko Suki (Kantai Collection -KanColle-) [English]", 
+        "https://www.luscious.net/albums/teitoku-wa-semai-toko-suki-kantai-collection-kanco_363520/"
+    );
+    let chapter1 = &mut Chapter::from(&comic1);
     extr.fetch_pages_unsafe(chapter1).unwrap();
     assert_eq!(
         "Teitoku wa Semai Toko Suki (Kantai Collection -KanColle-) [English]",
         chapter1.title
     );
     assert_eq!(26, chapter1.pages.len());
+    // let comics = extr.search("Teitoku wa Semai Toko Suki").unwrap();
+    // assert!(comics.len() > 0);
+    // assert_eq!(comics[0].title, comic1.title);
+    // assert_eq!(comics[0].url, comic1.url);
 }
