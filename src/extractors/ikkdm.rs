5dddb0f7eab2b9bb7423cdee0fde58c372aad2e1
Hentioe <me@bluerain.io>
2020-04-06 13:37:50 -0400

    Fix #52

--- src/extractors/ikkdm.rs
+++ src/extractors/ikkdm.rs
@@ -2,36 +2,36 @@ use super::*;
 use url::form_urlencoded::byte_serialize;
 
 def_regex2![
-    NAME        => r#"(.+)\[\d+\]?$"#,
-    NAME2       => r#"(.+)漫画(电信|联通)$"#,
+    INDEX_NAME  => r#"(.+)\[\d+\]?$"#,
+    SEARCH_NAME => r#"(.+)漫画在线$"#,
     TITLE       => "共(\\d+)页",
-    URL         => r#"(https?://comic\.ikkdm\.com/comiclist/\d+/\d+)/\d+\.htm"#,
+    URL         => r#"(https?://comic\.(ikkdm|kkkkdm)\.com/comiclist/\d+/\d+)/\d+\.htm"#,
     IMG         => r#"(?i)src='"\+.+\+"([^']+)'"#
 ];
 
-/// 对 comic.ikkdm.com 内容的抓取实现
+/// 对 comic.kkkkdm.com 内容的抓取实现
 /// 未来计划：
 /// - 可选择联通/电信
 def_extractor! {
     status	=> [
-        usable: true, pageable: false, searchable: true, http: true,
-        favicon: "http://comic.ikkdm.com/favicon.ico"
+        usable: true, pageable: false, searchable: true, http: true, pageable_search: true,
+        favicon: "http://comic.kkkkdm.com/favicon.ico"
     ],
     tags	=> [Chinese],
 
     fn index(&self, _page: u32) -> Result<Vec<Comic>> {
-        let url = "http://comic.ikkdm.com/top100.htm";
+        let url = "http://comic.kkkkdm.com/top100.htm";
 
         let mut comics = itemsgen2!(
             url             = url,
             parent_dom      = "#comicmain > dd",
             cover_dom       = "a > img",
             link_dom        = "a:nth-child(2)",
-            link_prefix     = "http://comic.ikkdm.com",
+            link_prefix     = "http://comic.kkkkdm.com",
             encoding        = GBK
         )?;
         comics.iter_mut().for_each(|c: &mut Comic| {
-            if let Ok(title) = match_content2!(&c.title, &*NAME_RE) {
+            if let Ok(title) = match_content2!(&c.title, &*INDEX_NAME_RE) {
                 c.title = title
             }
         });
@@ -39,10 +39,10 @@ def_extractor! {
         Ok(comics)
     }
 
-    fn search(&self, keywords: &str) -> Result<Vec<Comic>> {
+    fn paginated_search(&self, keywords: &str, page: u32) -> Result<Vec<Comic>> {
         let keywords_bytes = &encode_text(keywords, GBK)?[..];
         let keywords_encoded: String = byte_serialize(keywords_bytes).collect();
-        let url = format!("http://so.kukudm.com/search.asp?kw={}", keywords_encoded);
+        let url = format!("http://so.kukudm.com/search.asp?kw={}&page={}", keywords_encoded, page);
 
         let mut comics = itemsgen2!(
             url             = &url,
@@ -52,7 +52,8 @@ def_extractor! {
             encoding        = GBK
         )?;
         comics.iter_mut().for_each(|c: &mut Comic| {
-            if let Ok(title) = match_content2!(&c.title, &*NAME2_RE) {
+            c.url = c.url.replace("kukudm.com", "comic.kkkkdm.com");
+            if let Ok(title) = match_content2!(&c.title, &*SEARCH_NAME_RE) {
                 c.title = title
             }
         });
@@ -64,7 +65,7 @@ def_extractor! {
         itemsgen2!(
             url             = &comic.url,
             target_dom      = "#comiclistn > dd > a:nth-child(1)",
-            link_prefix     = "http://comic.ikkdm.com",
+            link_prefix     = "http://comic.kkkkdm.com",
             encoding        = GBK
         )?.attach_to(comic);
 
@@ -105,18 +106,18 @@ fn test_extr() {
     if extr.is_usable() {
         let comics = extr.index(1).unwrap();
         assert_eq!(100, comics.len());
-        let mut comic1 = Comic::new("妖精的尾巴", "http://kukudm.com/comiclist/346/");
-        extr.fetch_chapters(&mut comic1).unwrap();
-        assert_eq!(652, comic1.chapters.len());
+        let comic1 = &mut Comic::new("妖精的尾巴", "http://comic.kkkkdm.com/comiclist/346/");
+        extr.fetch_chapters(comic1).unwrap();
+        assert_eq!(654, comic1.chapters.len());
         let chapter1 = &mut comic1.chapters[3];
         extr.fetch_pages_unsafe(chapter1).unwrap();
         assert_eq!("妖精的尾巴 4话", chapter1.title);
         assert_eq!(20, chapter1.pages.len());
-        let chapter2 = &mut Chapter::from_link("", "http://comic.ikkdm.com/comiclist/2843/75129/1.htm");
+        let chapter2 = &mut Chapter::from_url("http://comic.kkkkdm.com/comiclist/2843/75129/1.htm");
         extr.fetch_pages_unsafe(chapter2).unwrap();
         assert_eq!("不死勇者罗曼史 5话", chapter2.title);
         assert_eq!(20, chapter2.pages.len());
-        let comics = extr.search("妖精的尾巴").unwrap();
+        let comics = extr.paginated_search("妖精的尾巴", 1).unwrap();
         assert!(comics.len() > 0);
         assert_eq!(comics[0].title, comic1.title);
         assert_eq!(comics[0].url, comic1.url);
