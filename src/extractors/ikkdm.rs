dadc0ba4cee96df01ab14225b0272caade505d4d
Hentioe <me@bluerain.io>
2020-05-20 01:53:27 +0800

    Fix kkkdm addresses

--- src/extractors/ikkdm.rs
+++ src/extractors/ikkdm.rs
@@ -6,10 +6,12 @@ def_regex2![
     SEARCH_NAME => r#"(.+)漫画在线$"#,
     TITLE       => "共(\\d+)页",
     URL         => r#"(https?://comic\.(ikkdm|kkkkdm)\.com/comiclist/\d+/\d+)/\d+\.htm"#,
-    IMG         => r#"(?i)src='"\+.+\+"([^']+)'"#
+    IMG         => r#"(?i)src='"\+.+\+"([^']+)'>(</a>|")"#
 ];
 
 // 对 comic.kkkkdm.com 内容的抓取实现
+// 可优化：
+// - 利用部分章节页面中存在的下一页地址
 // 未来计划：
 // - 可选择联通/电信
 def_extractor! {
@@ -75,6 +77,7 @@ def_extractor! {
     fn pages_iter<'a>(&'a self, chapter: &'a mut Chapter) -> Result<ChapterPages> {
         let pure_url = match_content2!(&chapter.url, &*URL_RE)?;
         chapter.url = format!("{}/1.htm", pure_url);
+
         let html = get(&chapter.url)?.decode_text(GBK)?;
         let document = parse_document(&html);
         chapter.set_title(document.dom_text("title")?);
@@ -93,7 +96,7 @@ def_extractor! {
             let page_url = format!("{}/{}.htm", pure_url, current_page);
             let page_html = get(&page_url)?.decode_text(GBK)?;
             let address = fetch_page(&page_html)?;
-            Ok(vec![Page::new(current_page - 1, address)])
+            Ok(vec![Page::new(current_page, address)])
         });
 
         Ok(ChapterPages::new(chapter, page_counut as i32, vec![first_address], fetch))
@@ -108,7 +111,7 @@ fn test_extr() {
         assert_eq!(100, comics.len());
         let comic1 = &mut Comic::new("妖精的尾巴", "http://comic.kkkkdm.com/comiclist/346/");
         extr.fetch_chapters(comic1).unwrap();
-        assert_eq!(654, comic1.chapters.len());
+        assert_eq!(661, comic1.chapters.len());
         let chapter1 = &mut comic1.chapters[3];
         extr.fetch_pages_unsafe(chapter1).unwrap();
         assert_eq!("妖精的尾巴 4话", chapter1.title);
@@ -117,6 +120,7 @@ fn test_extr() {
         extr.fetch_pages_unsafe(chapter2).unwrap();
         assert_eq!("不死勇者罗曼史 5话", chapter2.title);
         assert_eq!(20, chapter2.pages.len());
+
         let comics = extr.paginated_search("妖精的尾巴", 1).unwrap();
         assert!(comics.len() > 0);
         assert_eq!(comics[0].title, comic1.title);
